2c361cf5c12d6e5ae0842b1bc36e915b62fd8673
miquéias <jvnqdazuiera@gmail.com>
2026-09-18 15:38:38 -0300

    adicionei um popup para feedback durante a conversão de imagens e ajustei tipos de arquivo suportados

--- app.py
+++ app.py
@@ -50,13 +50,50 @@ class App(ctk.CTk):
         caminho = ctk.filedialog.askopenfilename(
             title="Selecione a imagem",
             filetypes=[
-                ("Arquivos de Imagem", ("*.png", "*.jpg", "*.jpeg", "*.webp", "*.tiff", "*.pdf", "*.bmp", "*.ico", "*.gif")),
+                ("Arquivos de Imagem", ("*.png", "*.jpg", "*.jpeg", "*.webp", "*.tiff", "*.bmp", "*.ico", "*.gif")),
                 ("Todos os arquivos", "*.*")
                 ]
         )
         if caminho:
             self.caminho_img_selecionada = caminho
             self.lbl_arquivo_imagem.configure(text=caminho)
+
+    def popup(self, mensagem, tempo_ms=3000, cor_fundo="darkgrey"):
+
+        if hasattr(self, "popup_ativo") and self.popup_ativo is not None:
+            try:
+                self.popup_ativo.destroy()
+            except Exception:
+                pass
+
+        self.popup_ativo = ctk.CTkFrame(self, fg_color=cor_fundo, corner_radius=10)
+        self.popup_ativo.place(relx=0.98, rely=0.95, anchor="se")
+
+        lbl = ctk.CTkLabel(self.popup_ativo, text=mensagem, font=("Arial", 13, "bold"), text_color="white")
+        lbl.pack(expand=True, fill="both", padx=15, pady=(10,5))
+
+        barra = ctk.CTkProgressBar(self.popup_ativo, height=3, progress_color="white", mode="determinate")
+        barra.pack(fill="x", side="bottom", padx=10, pady=(0, 8))
+        barra.set(0.0)
+
+        intervalo = 16
+        passos = tempo_ms / intervalo
+        incremento = 1.0 / passos
+
+        def atualizar_barra():
+            if not hasattr(self, "popup_ativo") or self.popup_ativo is None or not barra.winfo_exists():
+                return
+
+            valor_atual = barra.get()
+            if valor_atual < 1.0:
+                barra.set(valor_atual + incremento)
+                self.after(intervalo, atualizar_barra)
+            else:
+                self.popup_ativo.destroy()
+                self.popup_ativo = None
+
+        atualizar_barra()
+
     def executar_conversao_img(self):
         if not self.caminho_img_selecionada:
             print("Erro! Nenhum arquivo/ficheiro selecionado. . .")
@@ -65,9 +102,9 @@ class App(ctk.CTk):
         imagem_selecionada = self.caminho_img_selecionada
         nome_imagem, extensao = os.path.splitext(imagem_selecionada)
         imagem_nova = nome_imagem + '.' + formato.lower()
-        print("Convertendo imagem, aguarde..!")
+        self.popup(mensagem="Convertendo imagem, aguarde..!", tempo_ms=3000)
         converter_imagem(imagem_selecionada, imagem_nova)
-        print("Imagem convertida com sucesso!!")
+        self.popup(mensagem="Imagem convertida com sucesso!!", tempo_ms=3000 )
         
 
 
